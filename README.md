1dc6c49fb11c639a33401b9af858a286290e6d85
jazeem-azeez <jazeem10@gmail.com>
2019-07-24 21:05:33 +0530

    added logger

---
+++ README.md
@@ -0,0 +1,1 @@
+Sample ET_tool
---
+++ Src/ET_Tool.Common/Implementations/Logger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ET_Tool.Common.Implementations
+{
+	public class Logger
+	{
+
+	}
+}
